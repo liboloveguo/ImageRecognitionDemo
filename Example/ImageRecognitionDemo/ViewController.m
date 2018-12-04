c5326409876cf8680134d5fda79ecf56c673991e
张利果 <zhang.liguo@icerno.com>
2018-12-04 21:18:51 +0800

    提交文件

--- Example/ImageRecognitionDemo/ViewController.m
+++ Example/ImageRecognitionDemo/ViewController.m
@@ -5,15 +5,12 @@
 //  Created by 张利果 on 2018/11/21.
 //  Copyright © 2018年 张利果. All rights reserved.
 //
-#import "InfoDisplayViewController.h"
-//#import <AVFoundation/AVFoundation.h>
+
 #import "ImageRecognition.h"
 #import "ViewController.h"
 
 @interface ViewController ()<UIAlertViewDelegate>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) NSMutableArray<NSArray<NSString *> *> *actionList;
-@property (nonatomic,strong) UIImage *images;
+@property (nonatomic,strong) UITextView *textView;
 @end
 
 @implementation ViewController
@@ -22,125 +19,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //
-    self.actionList = [NSMutableArray array];
+    [self UI];
     
-//    [self.actionList addObject:@[@"通用文字识别（基础版、不含位置信息）", @"detectTextBasicFromImage"]];
+    [self imageCallBack];
     
-}
-
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
-    
-    if (error) {
-        
-        NSLog(@"保存失败");
-    }
-    else  {
-        NSLog(@"保存成功");
-    }
-    
-}
-
-#pragma mark - OCR识别返回结果处理
--(void)imageRecognitionType {
-    __weak typeof(self) weakSelf = self;
-    
-    [[ImageRecognition sharedInstance] imageRecognitionGeneralBasicVC:self successHandler:^(id result, UIImage *image) {
-        
-        //处理数据
-        NSMutableString *message = [NSMutableString string];
-        if(result[@"words_result"]){
-            if([result[@"words_result"] isKindOfClass:[NSDictionary class]]){
-                [result[@"words_result"] enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-                    if([obj isKindOfClass:[NSDictionary class]] && [obj objectForKey:@"words"]){
-                        [message appendFormat:@"%@: %@\n", key, obj[@"words"]];
-                    }else{
-                        [message appendFormat:@"%@: %@\n", key, obj];
-                    }
-                    
-                }];
-            }else if([result[@"words_result"] isKindOfClass:[NSArray class]]){
-                for(NSDictionary *obj in result[@"words_result"]){
-                    if([obj isKindOfClass:[NSDictionary class]] && [obj objectForKey:@"words"]){
-                        [message appendFormat:@"%@\n", obj[@"words"]];
-                    }else{
-                        [message appendFormat:@"%@\n", obj];
-                    }
-                    
-                }
-            }
-            
-        }else{
-            [message appendFormat:@"%@", result];
-        }
-        
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            [weakSelf dismissViewControllerAnimated:YES completion:nil];
-            //到主线程处理
-            if (message.length>0) {
-                InfoDisplayViewController *vc = [InfoDisplayViewController controller];
-                vc.image = image;
-                vc.info = message;
-                [weakSelf.navigationController pushViewController:vc animated:YES];
-            }
-            else{
-                //识别无内容
-                [[[UIAlertView alloc] initWithTitle:nil message:@"图片内容空空如也～～" delegate:weakSelf cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
-            }
-            
-        }];
-        
-        
-    } failHandler:^(NSError *error) {
-        //识别失败
-        NSString *msg = [NSString stringWithFormat:@"%li:%@", (long)[error code], [error localizedDescription]];
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            [weakSelf dismissViewControllerAnimated:YES completion:nil];
-            NSLog(@"%@",error);
-            [[[UIAlertView alloc] initWithTitle:@"识别失败" message:msg delegate:weakSelf cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
-        }];
-        
-    }];
-}
-
-#pragma mark UIAlertViewDelegate
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-#pragma mark - UITableViewDataSource
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.actionList.count;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-    cell.textLabel.text = self.actionList[indexPath.row][0];
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-   
-    [self imageRecognitionType];
-}
-
--(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    return CGFLOAT_MIN;
-}
--(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
-    return CGFLOAT_MIN;
-}
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
-    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
-        [cell setSeparatorInset:UIEdgeInsetsZero];
-    }
-    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
-        [cell setLayoutMargins:UIEdgeInsetsZero];
-    }
-}
-
-#pragma mark - UITableViewDelegate
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 50;
 }
 
 -(void)imageCallBack {
@@ -180,13 +62,27 @@
         
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             [weakSelf dismissViewControllerAnimated:YES completion:nil];
+            weakSelf.textView.text = message;
             NSLog(@"%@",message);
         }];
         
     } failHandler:^(NSError *err) {
-        NSLog(@"%@",err);
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [weakSelf dismissViewControllerAnimated:YES completion:nil];
+            NSLog(@"%@",err);
+        }];
+        
     }];
     
+}
+
+-(void) UI {
+    self.textView = [[UITextView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width-150, [UIScreen mainScreen].bounds.size.height*0.5)];
+    self.textView.font = [UIFont systemFontOfSize:15];
+    self.textView.textColor = [UIColor blackColor];
+    self.textView.center = self.view.center;
+    [self.view addSubview:self.textView];
 }
 
 @end
